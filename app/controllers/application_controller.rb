2924ecb652c88f9705e86b3ce6cc9214433329a6
EleAco <eleaco0128@gmail.com>
2020-08-21 19:02:24 +0900

    ユーザー管理機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,25 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :move_to_index, except: [:show, :new,:create]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
 
-  private
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, :remenber_created_at, :created_at, :updated_at])
+  end
   private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_registration_path
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :password, presence: true
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :password, length: { minimum: 6 }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :birthday, presence: true
 end
+
