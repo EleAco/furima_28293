23423dad9a4134faede671fadd015ad83cb1f58c
EleAco <eleaco0128@gmail.com>
2020-09-01 20:16:33 +0900

    商品購入機能3

--- app/forms/token_buy.rb
+++ app/forms/token_buy.rb
@@ -2,7 +2,6 @@ class TokenBuy
   include ActiveModel::Model
   attr_accessor :token, :zip_code, :area_id, :city, :addoress, :building_name, :phone_number, :user_id, :item_id
 
-  # モデルbuyから持ってきたバリデーション
   with_options presence: true do |i|
     i.validates :zip_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     i.validates :area_id, numericality: { other_than: 1 }
@@ -10,12 +9,6 @@ class TokenBuy
     i.validates :addoress
     i.validates :phone_number, format: { with: /\A\d{11}\z/ }
   end
-
-  # item_parchaseから持ってきたバリデーション
-  # with_options presence: true do |i|
-  #   i.validates :user_id
-  # i.validates :item_id
-  # end
 
   def save
     @item_purchase = ItemPurchase.create(user_id: user_id, item_id: item_id)
